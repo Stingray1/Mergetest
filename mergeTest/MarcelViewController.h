13dd8ab4bef2d7403b28b83e1901c19b1b00bdf9
Marcel <marcel@Marcels-MacBook-Pro.local>
2017-02-02 11:38:15 +0300

    marcel commit

---
+++ mergeTest/MarcelViewController.h
@@ -0,0 +1,13 @@
+//
+//  MarcelViewController.h
+//  mergeTest
+//
+//  Created by Marcel  on 2/2/17.
+//  Copyright © 2017 Vadim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MarcelViewController : UIViewController
+
+@end
