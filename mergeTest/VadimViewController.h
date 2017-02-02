76b17ce687e4cc34e4ef176500911974c503b05f
Stingray1 <plasvad94@gmail.com>
2017-02-02 11:06:17 +0200

    3 commit

---
+++ mergeTest/VadimViewController.h
@@ -0,0 +1,13 @@
+//
+//  VadimViewController.h
+//  mergeTest
+//
+//  Created by Vadim on 2/2/17.
+//  Copyright © 2017 Vadim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VadimViewController : UIViewController
+
+@end
